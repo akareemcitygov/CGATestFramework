36ef4f1c6b2ea2e072b49657ab468ca0e99ff257
Abdul Kareem <akareem@citygovapp.com>
2021-12-21 17:32:44 +0500

    add framework

---
+++ CGA.xcframework/ios-arm64/CGA.framework/Headers/CGA.h
@@ -0,0 +1,18 @@
+//
+//  CGA.h
+//  CGA
+//
+//  Created by Abdul Kareem on 12/14/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CGA.
+FOUNDATION_EXPORT double CGAVersionNumber;
+
+//! Project version string for CGA.
+FOUNDATION_EXPORT const unsigned char CGAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGA/PublicHeader.h>
+
+
